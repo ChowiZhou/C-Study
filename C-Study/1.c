27bc9cfc8b275028bc2f5d6fec8796cfd1c9e9d1
朝邑 周 <zhouzhaoyi666@outlook.com>
2022-07-07 02:37:42 +0800

    1

--- C-Study/1.c
+++ C-Study/1.c
@@ -1,12 +1,21 @@
 #include <stdio.h> /*包含一个文件*/
-int main(void)
+int main(void) /*函数名*/
 {
 	int num; /*定义一个num的变量*/
-	num = 1;
+	int door;
+	int color;
+	num = 1; /*给num赋予一个值*/
+	door = 5;
+	color = 10;
 
-	printf("I am a simple");
-	printf("computer.\n");
-	printf("My favorite number is %d because it is first.\n", num);
+	printf("I am a simple "); /*使用prinft()函数*/
+	printf("computer.\n"); /*/n作用为另起一行*/
+	printf("My favorite number is %d because it is first.\n", num);/*把num的值嵌到双引号内，%d为嵌入位置*/
+	/*以下为拓展练习*/
+	printf("我有%d苹果.\n", num);
+	printf("你有几个苹果.\n"); 
+	printf("你有%d个人.\n", door);
+	printf("I have %d apple.\n", color);
 
 	return 0;
 }
